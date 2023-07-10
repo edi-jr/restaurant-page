ac222603a7c9067c2b382da4feea4326116c2c31
edi <128224911+edi-jr@users.noreply.github.com>
2023-07-10 20:45:31 -0300

    create menu section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.css";
 import loadNav from "./modules/nav";
 import loadHome from "./modules/home";
+import loadMenu from "./modules/menu";
 
 loadNav();
 loadHome();
---
+++ src/modules/menu.js
@@ -0,0 +1,28 @@
+function createCard(title) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = "<span>🍜</span>";
+  card.innerHTML += `<h2>${title}</h2>`;
+  card.innerHTML += "<p>Pork chashu, menma, nori, negi, boiled egg, corn</p>";
+  card.innerHTML += "<p>$20</p>";
+  return card;
+}
+
+function createMenuSection() {
+  const menu = document.createElement("section");
+  menu.classList.add("menu");
+  menu.innerHTML = "<h1>Our menu:</h1>";
+  menu.appendChild(createCard("Shio ramen"));
+  menu.appendChild(createCard("Shoyu ramen"));
+  menu.appendChild(createCard("Miso ramen"));
+  menu.appendChild(createCard("Curry ramen"));
+  return menu;
+}
+
+function loadMenu() {
+  const content = document.querySelector("#content");
+  const menu = createMenuSection();
+  content.appendChild(menu);
+}
+
+export default loadMenu;
